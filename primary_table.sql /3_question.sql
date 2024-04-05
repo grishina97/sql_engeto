8978f93e299ef5ddae6fa878a52d4695f33867eb
Anastasiia Grishina <149105805+grishina97@users.noreply.github.com>
2024-04-05 14:16:05 +0200

    Update 3_question.sql

--- primary_table.sql /3_question.sql
+++ primary_table.sql /3_question.sql
@@ -3,12 +3,12 @@ SELECT
     concat ( round( ((b.avg_price_2018 - a.avg_price_2006) / a.avg_price_2006) * 100 ) , '%') AS Price_appreciation
 FROM
     (SELECT products, AVG(price) AS avg_price_2006
-     FROM t_Anastasiia_Grishina_project_SQL_primarni_final  
+     FROM t_Anastasiia_Grishina_project_SQL_primary_final  
      WHERE YEAR = 2006
      GROUP BY products) AS a
 JOIN
     (SELECT products, AVG(price) AS avg_price_2018
-     FROM t_Anastasiia_Grishina_project_SQL_primarni_final  
+     FROM t_Anastasiia_Grishina_project_SQL_primary_final  
      WHERE YEAR = 2018
      GROUP BY products) AS b 
 ON a.products = b.products
@@ -19,12 +19,12 @@ SELECT
     concat ( round( (b.avg_price_2018 - a.avg_price_2006) * 100 / (b.avg_price_2018 + a.avg_price_2006) ) , '%') AS Rise_in_price
 FROM
     (SELECT products, AVG(price) AS avg_price_2006
-     FROM t_Anastasiia_Grishina_project_SQL_primarni_final  
+     FROM t_Anastasiia_Grishina_project_SQL_primary_final  
      WHERE YEAR = 2006
      GROUP BY products) AS a
 JOIN
     (SELECT products, AVG(price) AS avg_price_2018
-     FROM t_Anastasiia_Grishina_project_SQL_primarni_final  
+     FROM t_Anastasiia_Grishina_project_SQL_primary_final  
      WHERE YEAR = 2018
      GROUP BY products) AS b 
 ON a.products = b.products
