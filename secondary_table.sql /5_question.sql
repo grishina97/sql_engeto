2adcb3dcae87e36fa2f69ae9a2b8919a2a81c376
Anastasiia Grishina <149105805+grishina97@users.noreply.github.com>
2024-04-05 14:16:40 +0200

    Update 5_question.sql

--- secondary_table.sql /5_question.sql
+++ secondary_table.sql /5_question.sql
@@ -1,6 +1,6 @@
 CREATE TABLE t_Anastasiia_Grishina_project_SQL_secondary_final AS 
 SELECT b.YEAR AS Year, round(avg(a.GDP/1000000000)) AS HDP, round(avg(b.price)) AS Price, round(avg(b.payroll)) AS Payroll
 FROM economies a
-JOIN t_Anastasiia_Grishina_project_SQL_primarni_final b ON a.YEAR = b.year 
+JOIN t_Anastasiia_Grishina_project_SQL_primary_final b ON a.YEAR = b.year 
 GROUP BY YEAR
 ORDER BY YEAR;
