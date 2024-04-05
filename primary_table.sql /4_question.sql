2d36059f67638e3f3ff8caf2f071db685407ef3b
Anastasiia Grishina <149105805+grishina97@users.noreply.github.com>
2024-04-05 14:14:07 +0200

    Update 4_question.sql

--- primary_table.sql /4_question.sql
+++ primary_table.sql /4_question.sql
@@ -1,8 +1,17 @@
 SELECT 
-	B.YEAR, 
-	concat( round( (A.price - B.price) / B.price * 100), '%') AS Rise_in_price,
-	concat( round( (A.payroll - B.payroll) / B.payroll * 100), '%') AS Rise_in_payroll
-FROM t_Anastasiia_Grishina_project_SQL_primarni_final A
-LEFT JOIN t_Anastasiia_Grishina_project_SQL_primarni_final B ON A.YEAR = B.YEAR + 1
+    B.YEAR, 
+    concat( round( (A.price - B.price) / B.price * 100), '%') AS Rise_in_price,
+    concat( round( (A.payroll - B.payroll) / B.payroll * 100), '%') AS Rise_in_payroll
+FROM (
+    SELECT avg(price) AS price, YEAR, avg(payroll) AS payroll
+    FROM t_Anastasiia_Grishina_project_SQL_primarni_final 
+    GROUP BY year
+    ) AS A
+LEFT JOIN (
+    SELECT avg(price) AS price, YEAR, avg(payroll) AS payroll
+    FROM t_Anastasiia_Grishina_project_SQL_primarni_final 
+    GROUP BY year
+    ) AS B 
+        ON A.YEAR = B.YEAR + 1
 GROUP BY YEAR
 ORDER BY YEAR;
